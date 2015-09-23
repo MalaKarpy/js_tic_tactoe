56da1e179a2595737aa02a8fabf341cdf860f753
diana & mala <student@epicodus.com>
2015-09-23 13:47:49 -0700

    Add passing specs for getMark and setMark functions

--- spec/specs.js
+++ spec/specs.js
@@ -1,6 +1,34 @@
-describe('getScore', function() {
-  it("is 13 for getScore [A, 2]", function() {
-    var deck = ['A', '2'];
-    expect(getScore(deck)).to.equal(13);
+describe('Player', function() {
+  it("returns the player's mark", function() {
+    var testPlayer = new Player("X");
+    expect(testPlayer.mark).to.equal("X");
   });
+});
+
+describe('Space', function() {
+  it("returns the correct x-coordinate", function() {
+    var testSpace = new Space(1,2);
+    expect(testSpace.x).to.equal(1);
+  });
+
+  it("returns the correct y-coordinate", function() {
+    var testSpace = new Space(1,2);
+    expect(testSpace.y).to.equal(2);
+  })
+
+  it("lets a player mark a space", function() {
+      var testPlayer = new Player("X");
+      var testSpace = new Space(1,2);
+      testSpace.setMark(testPlayer)
+      expect(testSpace.getMark()).to.equal(testPlayer);
+   });
+
+   it("assigns a mark to the correct space", function() {
+       var testPlayer = new Player("X");
+       var otherPlayer = new Player("O");
+       var testSpace = new Space(1,2);
+       testSpace.setMark(testPlayer);
+       expect(testSpace.getMark()).to.not.equal(otherPlayer);
+    });
+
 });
