a2aef3f5452c7af170862b245d93bb0d7eda370c
diana & mala <student@epicodus.com>
2015-09-23 14:25:13 -0700

    Add behavior for board

--- js/scripts.js
+++ js/scripts.js
@@ -27,3 +27,8 @@ Space.prototype.setMark = function(mark) {
 Space.prototype.getMark = function() {
   return this.mark; // or do this.state = mark, return this, space= space.setMark(x)
 }
+
+function Board() {
+  var board = [{x:1,y:1}, {x:1,y:2}, {x:1,y:3}, {x:2,y:1}, {x:2,y:2}, {x:2,y:3}, {x:3,y:1}, {x:3,y:2}, {x:3,y:3}];
+  return board;
+}
